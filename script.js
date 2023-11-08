c86dfd74c408a79f7387757bc27d5a112cc2fec8
wnnow <117201813+wnnow@users.noreply.github.com>
2023-11-08 16:08:49 +0700

    Add html css skeleton header & dummy content card

---
+++ script.js
@@ -0,0 +1,11 @@
+const library = [];
+const addBookBtn = document.querySelector(".add-book-btn");
+
+function Book(name, author, page, readStatus) {
+  this.name = name;
+  this.author = author;
+  this.page = page;
+  this.readStatus = readStatus;
+}
+
+function addBookToLibrary() {}
