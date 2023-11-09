40afb14fd18921b2fce612dd8b5a0965de107c4f
wnnow <117201813+wnnow@users.noreply.github.com>
2023-11-09 18:29:43 +0700

    Add toggle update reading status function style

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const library = [];
 const addBookBtn = document.querySelector(".add-book-btn");
 const form = document.querySelector(".form-input");
+const readingCheckboxSpans = Array.from(
+  document.querySelectorAll(".toggle-reading-checkbox")
+);
+const library = [];
 
 function toggleDisplayForm() {
   if (form.style.display === "" || form.style.display === "block") {
@@ -10,6 +13,43 @@ function toggleDisplayForm() {
   }
 }
 
+function toggleReadingDataSet(e, dataset) {
+  if (dataset === "true") {
+    e.dataset.read = "false";
+  } else {
+    e.dataset.read = "true";
+  }
+}
+
+function toggleReadingStatusText(e, dataset) {
+  if (dataset === "true") {
+    e.textContent = "Unread";
+  } else {
+    e.textContent = "Read";
+  }
+}
+
+function toggleReadingStatusCheckBox(e, dataset) {
+  if (dataset === "false") {
+    e.target.textContent = "check_box";
+  } else {
+    e.target.textContent = "check_box_outline_blank";
+  }
+}
+function updateReadingStatus(e) {
+  let card = e.target.closest(".card");
+  let dataset = card.dataset.read;
+  let readingStatusText = e.target.closest(
+    'button[type="button"]'
+  ).nextElementSibling;
+
+  toggleReadingDataSet(card, dataset);
+  toggleReadingStatusCheckBox(e, dataset);
+  toggleReadingStatusText(readingStatusText, dataset);
+}
+readingCheckboxSpans.forEach((e) =>
+  e.addEventListener("click", updateReadingStatus)
+);
 addBookBtn.addEventListener("click", toggleDisplayForm);
 
 function Book(name, author, page, readStatus) {
