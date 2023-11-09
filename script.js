f8f444f7e73c7066bfacf95db984b8163fd4fa43
wnnow <117201813+wnnow@users.noreply.github.com>
2023-11-09 16:20:24 +0700

    Add toggle form input function

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const library = [];
 const addBookBtn = document.querySelector(".add-book-btn");
+const form = document.querySelector(".form-input");
+
+function toggleDisplayForm() {
+  if (form.style.display === "" || form.style.display === "block") {
+    form.style.display = "none";
+  } else {
+    form.style.display = "block";
+  }
+}
+
+addBookBtn.addEventListener("click", toggleDisplayForm);
 
 function Book(name, author, page, readStatus) {
   this.name = name;
