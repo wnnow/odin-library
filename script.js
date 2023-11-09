65b4d3e52eee1cc8fc956ae543c784c954b25f6c
wnnow <117201813+wnnow@users.noreply.github.com>
2023-11-09 23:37:25 +0700

    Add createCard, newBookSubmit Func

--- script.js
+++ script.js
@@ -3,13 +3,19 @@ const form = document.querySelector(".form-input");
 const readingCheckboxSpans = Array.from(
   document.querySelectorAll(".toggle-reading-checkbox")
 );
+const formBackBtn = document.querySelector("button.form-back-btn");
+const newbookFormSubmitBtn = document.querySelector(".form-submit-btn");
+
+const newBookform = document.querySelector("#new-book-form");
+const cardContainer = document.querySelector(".card-container");
 const library = [];
 
 function toggleDisplayForm() {
-  if (form.style.display === "" || form.style.display === "block") {
-    form.style.display = "none";
-  } else {
+  console.log(`ToggleDisplay!!`);
+  if (form.style.display === "" || form.style.display === "none") {
     form.style.display = "block";
+  } else {
+    form.style.display = "none";
   }
 }
 
@@ -36,27 +42,119 @@ function toggleReadingStatusCheckBox(e, dataset) {
     e.target.textContent = "check_box_outline_blank";
   }
 }
+
 function updateReadingStatus(e) {
   let card = e.target.closest(".card");
   let dataset = card.dataset.read;
-  let readingStatusText = e.target.closest(
-    'button[type="button"]'
-  ).nextElementSibling;
-
+  //   let readingStatusText = e.target.closest(
+  //     'button[type="button"]'
+  //   ).nextElementSibling;
+  let readingStatusText;
+  if (e.target === undefined) {
+    readingStatusText = e.closest('button[type="button"]').nextElementSibling;
+  } else {
+    readingStatusText = e.target.parentElement.nextElementSibling;
+  }
   toggleReadingDataSet(card, dataset);
   toggleReadingStatusCheckBox(e, dataset);
   toggleReadingStatusText(readingStatusText, dataset);
 }
+
 readingCheckboxSpans.forEach((e) =>
   e.addEventListener("click", updateReadingStatus)
 );
+// newbookFormSubmitBtn.addEventListener("click", toggleDisplayForm);
 addBookBtn.addEventListener("click", toggleDisplayForm);
+formBackBtn.addEventListener("click", toggleDisplayForm);
 
-function Book(name, author, page, readStatus) {
-  this.name = name;
+function Book(title, author, pages, readStatus) {
+  this.title = title;
   this.author = author;
-  this.page = page;
+  this.pages = pages;
   this.readStatus = readStatus;
 }
 
-function addBookToLibrary() {}
+function addBookToLibrary(book) {
+  library.push(book);
+}
+
+newbookFormSubmitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (form.checkValidity()) {
+    const bookTitle = document.querySelector("#book_title").value;
+    const bookAuthor = document.querySelector("#book_author").value;
+    const pages = document.querySelector("#book_page_no").value;
+    const readingStatus = document.querySelector(
+      'input[name="reading_status"]:checked'
+    ).value;
+    const book = new Book(bookTitle, bookAuthor, pages, readingStatus);
+
+    addBookToLibrary(book);
+  }
+});
+
+newBookform.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+function createCard(item) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  if (item.readStatus === "unread") {
+    card.setAttribute(`data-read`, `false`);
+  } else {
+    card.setAttribute(`data-read`, `true`);
+  }
+
+  const bookTitle = document.createElement("div");
+  bookTitle.classList.add("book-title");
+  bookTitle.textContent = `${item.title}`;
+
+  const bookAuthor = document.createElement("div");
+  bookAuthor.classList.add("book-author");
+  bookAuthor.textContent = `${item.author}`;
+
+  const pages = document.createElement("div");
+  pages.classList.add("pages");
+  pages.textContent = `${item.pages}`;
+
+  const readingStatus = document.createElement("div");
+  readingStatus.classList.add("reading-status");
+
+  const btnSpanCheckboxContainer = document.createElement("button");
+  btnSpanCheckboxContainer.setAttribute("type", "submit");
+
+  const btnSpanCheckbox = document.createElement("span");
+  btnSpanCheckbox.classList.add(
+    "material-symbols-outlined",
+    "toggle-reading-checkbox"
+  );
+  const readingStatusText = document.createElement("div");
+  readingStatusText.classList.add("reading-status-text");
+  if (item.readingStatus === "read") {
+    btnSpanCheckbox.textContent = `check_box`;
+    readingStatusText.textContent = "Read";
+  } else {
+    btnSpanCheckbox.textContent = `check_box_outline_blank`;
+    readingStatusText.textContent = "Unread";
+  }
+  btnSpanCheckboxContainer.append(btnSpanCheckbox);
+  readingStatus.appendChild(btnSpanCheckboxContainer);
+  readingStatus.appendChild(readingStatusText);
+  const btnClosingSpanContainer = document.createElement("button");
+  const closingSpan = document.createElement("span");
+
+  closingSpan.classList.add("material-symbols-outlined");
+  closingSpan.textContent = "close";
+  btnClosingSpanContainer.appendChild(closingSpan);
+  card.appendChild(bookTitle);
+  card.appendChild(bookAuthor);
+  card.appendChild(pages);
+  card.appendChild(readingStatus);
+  card.appendChild(btnClosingSpanContainer);
+  cardContainer.appendChild(card);
+
+  btnSpanCheckbox.addEventListener("click", updateReadingStatus);
+}
+
+library.forEach(createCard);
